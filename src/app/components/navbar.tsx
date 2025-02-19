5ee7a099aea7457176f1db26aefcd20bcc0b8fda
foeiieof <akarawat.chant@gmail.com>
2025-02-19 14:51:01 +0700

    update : style & add components#4

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="z-50 w-full bg-transparent h-[55px] flex flex-row justify-between items-center px-[15px] fixed">
       <div className="relative w-[50px] h-[50px] flex flex-col justify-center items-center bg-[#f4f4f4] rounded-full">
         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-zinc-400/30 opacity-25"></span>
-        <Image className="w-[40px] h-[40px] object-cover rounded-full object-bottom z-30" src={'/prof/prof.jpg'} alt="" width={35} height={35} />
+        <Image className="w-[40px] h-[40px] object-cover rounded-full object-bottom z-30" src={'/prof/profile.jpg'} alt="" width={35} height={35} />
       </div>
       <div className="w-[40px] h-[28px] rounded-full flex flex-col justify-center items-center bg-[#f4f4f4]">
         <IoChatbubbleEllipses
