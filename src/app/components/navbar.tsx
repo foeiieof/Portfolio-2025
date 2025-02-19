f6b15c5b61523d65860d373a4141a0463b33a504
foeiieof <akarawat.chant@gmail.com>
2025-02-19 14:47:39 +0700

    update : style & add components#3

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,13 +1,13 @@
 import { CgProfile } from "react-icons/cg";
 import { IoChatbubbleEllipses } from "react-icons/io5";
 import Image from 'next/image'
+
 const Navbar = () => {
   return (
     <nav className="z-50 w-full bg-transparent h-[55px] flex flex-row justify-between items-center px-[15px] fixed">
       <div className="relative w-[50px] h-[50px] flex flex-col justify-center items-center bg-[#f4f4f4] rounded-full">
         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-zinc-400/30 opacity-25"></span>
         <Image className="w-[40px] h-[40px] object-cover rounded-full object-bottom z-30" src={'/prof/prof.jpg'} alt="" width={35} height={35} />
-        {/* <CgProfile color="#232323" size={23} /> */}
       </div>
       <div className="w-[40px] h-[28px] rounded-full flex flex-col justify-center items-center bg-[#f4f4f4]">
         <IoChatbubbleEllipses
