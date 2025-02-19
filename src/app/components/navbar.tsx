1e9330e3df7dc7d83a86b8ab4fb89e14aaa2cc01
foeiieof <akarawat.chant@gmail.com>
2025-02-20 00:51:11 +0700

    update : project page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,29 +7,29 @@ import { CgChevronLeft } from "react-icons/cg";
 
 const Navbar = () => {
   const url = usePathname()
-  console.log('url:', url)
-  const [path, setPath] = useState(false)
+  // console.log('url:', url)
+  const [path, setPath] = useState<boolean>(true)
   const route = useRouter()
   useEffect(() => {
-    if (!path && url.length > 1) { setPath(true) }
-    else { setPath(false) }
+    if (url !== "/") { setPath(false) }
+    else { setPath(true) }
   }, [path, url])
 
   return (
     <nav className="z-50 w-full bg-transparent h-[55px] flex flex-row justify-between items-center px-[15px] fixed">
-      {path ?
+      {!path &&
         <div
           onClick={() => { route.push('/') }}
-          className="cursor-pointer w-[40px] h-[28px] rounded-full flex flex-col justify-center items-center bg-[#f4f4f4]">
+          className="cursor-pointer w-[40px] h-[28px] rounded-full flex flex-col justify-center items-center bg-[#f4f4f4] border md:border-0 border-zinc-200/50">
           <CgChevronLeft
             color="#7e7e7e" size={18} />
         </div>
-        : <></>}
-      <div className="relative w-[50px] h-[50px] flex flex-col justify-center items-center bg-[#f4f4f4] rounded-full">
+      }
+      <div className="relative w-[50px] h-[50px] flex flex-col justify-center items-center bg-[#f4f4f4] rounded-full border md:border-0 border-zinc-200/50">
         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-zinc-400/30 opacity-25"></span>
         <Image className="w-[40px] h-[40px] object-cover rounded-full object-bottom z-30" src={'/prof/profile.jpg'} alt="" width={35} height={35} />
       </div>
-      <div className="w-[40px] h-[28px] rounded-full flex flex-col justify-center items-center bg-[#f4f4f4]">
+      <div className="w-[40px] h-[28px] rounded-full flex flex-col justify-center items-center bg-[#f4f4f4] border md:border-0 border-zinc-200/50">
         <IoChatbubbleEllipses
           color="#7e7e7e" size={18} />
       </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,25 +1,59 @@
 import { CiDesktop, CiMobile2 } from "react-icons/ci";
+import Image from 'next/image'
 const Projects = () => {
   return (
-    <main className="container w-screen h-screen m-auto flex flex-col justify-center items-center">
-      <div className="w-full h-10 flex flex-row justify-between items-center">
-        <span className="text-[26px] font-bold">Projects</span>
-        <p className="text-[14px] font-medium text-black/30">Got an idea for a project? <span className="underline font-bold text-[16px] text-black">Let builds&apos;it</span></p>
+    <main className="container w-screen h-full m-auto flex flex-col justify-center items-center pt-12 pb-8 px-8  gap-4">
+
+      <div className="w-full h-10 max-h-8 flex flex-col md:flex-row justify-between items-center my-2">
+        <span className="md:text-[26px] font-bold">Projects</span>
+        <p className="text-[12px] md:text-[14px] font-medium text-black/30">Got an idea for a project ?
+          <span className="underline font-bold md:text-[16px] text-black">&nbsp;Let builds&apos;it</span></p>
       </div>
       {/* <span>Hello project Page !</span> */}
-      {/* core */}
-      <div className="flex flex-col justify-start items-center w-full h-[600px] bg-[#f4f4f4] p-4 rounded-xl">
+
+      {/* core project card */}
+      <div className="flex flex-col justify-start items-center w-full h-[440px] md:h-[600px] bg-[#f4f4f4] p-4 pb-0 rounded-xl">
         {/* icon */}
-        <div className="w-full flex items-center flex-row h-12 gap-2">
-          <div className="w-10 h-10 flex justify-center items-center bg-white/30 shadow-md rounded-full" >
-            <CiMobile2 size={25} className="text-black/30" />
+        <div className="w-full h-fit flex items-center flex-row gap-2">
+          <div className="w-8 h-8 flex justify-center items-center bg-white/30 shadow-md rounded-full" >
+            <CiMobile2 size={20} className="text-black/30" />
           </div>
-          <div className="w-10 h-10 flex justify-center items-center bg-white/30 shadow-md rounded-full" >
-            <CiDesktop size={25} className="text-black/30" />
+          <div className="w-8 h-8 flex justify-center items-center bg-white/30 shadow-md rounded-full" >
+            <CiDesktop size={20} className="text-black/30" />
+          </div>
+        </div>
+        <div className="flex flex-col justify-end items-center font-bold">
+          <span className="text-black/80 text-[24px]">Name :</span>
+          <span className="text-black/30 text-[20px]">desc :</span>
+        </div>
+        <div className="w-full h-full flex justify-center items-end">
+          <div className="relative w-[280px] h-[300px] flex justify-center items-end overflow-hidden">
+            <Image
+              className="w-full h-full max-w-[260px] max-h-[280px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
+              src={'/proj/proj_note.png'} width={220} height={260} alt='proj_note' />
           </div>
         </div>
 
+        {/* <div className="relative w-full h-full flex flex-col justify-end items-center hover:scale-[1.02] overflow-hidden"> */}
+        {/*   <Image */}
+        {/*     className=" w-[220px] h-[260px]  transition-all ease-in-out duration-[260ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.6)] hover:drop-shadow-[0px_0px_8px_rgba(0,0,0,0.65)]" */}
+        {/*     src={'/proj/proj_note.png'} width={180} height={180} alt='proj_note' /> */}
+        {/* </div> */}
       </div>
+
+      {/* core project card */}
+      <div className="flex flex-col justify-start items-center w-full h-[440px] md:h-[600px] bg-[#f4f4f4] p-4 rounded-xl">
+        {/* icon */}
+        <div className="w-full h-fit flex items-center flex-row gap-2">
+          <div className="w-8 h-8 flex justify-center items-center bg-white/30 shadow-md rounded-full" >
+            <CiMobile2 size={20} className="text-black/30" />
+          </div>
+          <div className="w-8 h-8 flex justify-center items-center bg-white/30 shadow-md rounded-full" >
+            <CiDesktop size={20} className="text-black/30" />
+          </div>
+        </div>
+      </div>
+
     </main>
   )
 }
