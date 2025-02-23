31aa9060b2339199c2b4dfd3e702a149c1cfd178
foeiieof <akarawat.chant@gmail.com>
2025-02-23 23:16:22 +0700

    update : project page#4

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -31,21 +31,21 @@ const Projects = () => {
         </div>
         <div className="w-full h-full flex justify-center items-end">
           {/* setting w,h for scale cover size img */}
-          <div className="group relative w-[280px] md:w-[800px] h-[300px] md:h-[450px] flex justify-center items-end overflow-hidden">
+          <div className="group relative w-[430px] md:w-[800px] h-[280px] md:h-[450px] flex justify-center items-end overflow-hidden">
             {/* setting w,h for size img */}
             <Image
               className="z-20 w-full h-full max-w-[260px] md:max-w-[360px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
               src={'/proj/proj_note.png'} width={220} height={260} alt='proj_note' />
             <Image
-              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[140px] h-[180px] bottom-0 right-20 rotate-[18deg] transition-transform duration-[250ms]"
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 md:group-hover:rotate-[8deg]  absolute w-[140px] h-[180px] bottom-0 right-20 rotate-[18deg] transition-transform duration-[250ms]"
               src={'/proj/proj_note_comp/comp_1.png'} width={160} height={160} alt=""
             />
             <Image
-              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[140px] h-[180px] -bottom-2 left-24 -rotate-[18deg] transition-transform duration-[250ms]"
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 md:group-hover:-rotate-[10deg]  absolute w-[140px] h-[180px] -bottom-2 left-24 -rotate-[18deg] transition-transform duration-[250ms]"
               src={"/proj/proj_note_comp/comp_2.png"} width={160} height={160} alt=""
             />
             <Image
-              className="blur-[1px] group-hover:blur-0 group-hover:scale-150 group-hover:-rotate-[0deg] absolute w-[220px] h-[90px] top-6 right-20 -rotate-[18deg] transition-transform duration-[250ms]"
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-150 md:group-hover:-rotate-[0deg]  absolute w-[100px] md:w-[220px] md:h-[90px] top-6 md:top-6 right-8 md:right-20 -rotate-[18deg] transition-transform duration-[250ms]"
               src={"/proj/proj_note_comp/comp_3.png"} width={160} height={160} alt=""
             />
           </div>
@@ -77,17 +77,17 @@ const Projects = () => {
         </div>
         <div className="w-full h-full flex justify-center items-end">
           {/* setting w,h for scale cover size img */}
-          <div className="group relative w-[280px] md:w-[800px] h-[300px] md:h-[450px] flex justify-center items-end overflow-hidden">
+          <div className="group relative w-[430px] md:w-[800px] h-[288px] md:h-[450px] flex justify-center items-end overflow-hidden">
             {/* setting w,h for size img */}
             <Image
               className="z-20 w-full h-full max-w-[260px] md:max-w-[360px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
               src={'/proj/proj_loyal.png'} width={220} height={260} alt='proj_note' />
             <Image
-              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[140px] h-[300px] bottom-0 right-20 rotate-[18deg] transition-transform duration-[250ms]"
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[100px] md:w-[140px]  h-[230px] md:h-[300px] bottom-0 right-12 md:right-20 rotate-[18deg] transition-transform duration-[250ms]"
               src={'/proj/proj_loyal_comp/comp_1.png'} width={160} height={160} alt=""
             />
             <Image
-              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[140px] h-[300px] -bottom-2 left-24 -rotate-[18deg] transition-transform duration-[250ms]"
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[100px] md:w-[140px] md:h-[300px] h-[230px] -bottom-2  left-12 md:left-24 -rotate-[18deg] transition-transform duration-[250ms]"
               src={"/proj/proj_loyal_comp/comp_2.png"} width={160} height={160} alt=""
             />
             {/* <Image */}
