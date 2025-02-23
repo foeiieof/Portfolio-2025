d37215941f99e9962a4d3bcdd4ebf226b55e35a7
foeiieof <akarawat.chant@gmail.com>
2025-02-23 22:49:47 +0700

    update : project page#3

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,6 @@
 import { CiDesktop, CiMobile2 } from "react-icons/ci";
 import Image from 'next/image'
+import { CgChevronRight } from "react-icons/cg";
 const Projects = () => {
   return (
     <main className="container w-screen h-full m-auto flex flex-col justify-center items-center pt-12 pb-8 px-8  gap-4">
@@ -23,16 +24,30 @@ const Projects = () => {
           </div>
         </div>
         <div className="flex flex-col justify-end items-center font-bold">
-          <span className="text-black/80 text-[24px]">Name :</span>
-          <span className="text-black/30 text-[20px]">desc :</span>
+          <span className="text-black/80 text-[24px]">Notos</span>
+          <span className="text-black/30 text-[20px]">take your second brain!</span>
+          <div className=""></div>
+          <div className="w-[48px] h-[30px] flex justify-center items-center bg-white rounded-2xl mt-2 hover:rotate-6 hover:shadow-card transition delay-icon duration-icon"><CgChevronRight size={24} color={"#7e7e7e"} /></div>
         </div>
         <div className="w-full h-full flex justify-center items-end">
           {/* setting w,h for scale cover size img */}
-          <div className="relative w-[280px] md:w-[400px] h-[300px] md:h-[360px] flex justify-center items-end overflow-hidden">
+          <div className="group relative w-[280px] md:w-[800px] h-[300px] md:h-[450px] flex justify-center items-end overflow-hidden">
             {/* setting w,h for size img */}
             <Image
-              className="w-full h-full max-w-[260px] md:max-w-[360px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
+              className="z-20 w-full h-full max-w-[260px] md:max-w-[360px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
               src={'/proj/proj_note.png'} width={220} height={260} alt='proj_note' />
+            <Image
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[140px] h-[180px] bottom-0 right-20 rotate-[18deg] transition-transform duration-[250ms]"
+              src={'/proj/proj_note_comp/comp_1.png'} width={160} height={160} alt=""
+            />
+            <Image
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[140px] h-[180px] -bottom-2 left-24 -rotate-[18deg] transition-transform duration-[250ms]"
+              src={"/proj/proj_note_comp/comp_2.png"} width={160} height={160} alt=""
+            />
+            <Image
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-150 group-hover:-rotate-[0deg] absolute w-[220px] h-[90px] top-6 right-20 -rotate-[18deg] transition-transform duration-[250ms]"
+              src={"/proj/proj_note_comp/comp_3.png"} width={160} height={160} alt=""
+            />
           </div>
         </div>
 
@@ -54,6 +69,34 @@ const Projects = () => {
             <CiDesktop size={20} className="text-black/30" />
           </div>
         </div>
+        <div className="flex flex-col justify-end items-center font-bold">
+          <span className="text-black/80 text-[24px]">WMember (Line)</span>
+          <span className="text-black/30 text-[20px]">Loyal Customer Support</span>
+          <div className=""></div>
+          <div className="w-[48px] h-[30px] flex justify-center items-center bg-white rounded-2xl mt-2 hover:rotate-6 hover:shadow-card transition delay-icon duration-icon"><CgChevronRight size={24} color={"#7e7e7e"} /></div>
+        </div>
+        <div className="w-full h-full flex justify-center items-end">
+          {/* setting w,h for scale cover size img */}
+          <div className="group relative w-[280px] md:w-[800px] h-[300px] md:h-[450px] flex justify-center items-end overflow-hidden">
+            {/* setting w,h for size img */}
+            <Image
+              className="z-20 w-full h-full max-w-[260px] md:max-w-[360px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
+              src={'/proj/proj_loyal.png'} width={220} height={260} alt='proj_note' />
+            <Image
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[140px] h-[300px] bottom-0 right-20 rotate-[18deg] transition-transform duration-[250ms]"
+              src={'/proj/proj_loyal_comp/comp_1.png'} width={160} height={160} alt=""
+            />
+            <Image
+              className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[140px] h-[300px] -bottom-2 left-24 -rotate-[18deg] transition-transform duration-[250ms]"
+              src={"/proj/proj_loyal_comp/comp_2.png"} width={160} height={160} alt=""
+            />
+            {/* <Image */}
+            {/*   className="blur-[1px] group-hover:blur-0 group-hover:scale-150 group-hover:-rotate-[0deg] absolute w-[220px] h-[90px] top-6 right-20 -rotate-[18deg] transition-transform duration-[250ms]" */}
+            {/*   src={"/proj/proj_note_comp/comp_3.png"} width={160} height={160} alt="" */}
+            {/* /> */}
+          </div>
+        </div>
+
       </div>
 
     </main>
