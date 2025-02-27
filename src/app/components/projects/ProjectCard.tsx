636adb03b630ded1331e3fa4034f7fc9b76ef97a
foeiieof <akarawat.chant@gmail.com>
2025-02-27 21:05:06 +0700

    update : proj card style

--- src/app/components/projects/ProjectCard.tsx
+++ src/app/components/projects/ProjectCard.tsx
@@ -36,17 +36,17 @@ export const ProjectCard = ({ header, desc, href, mainimg, leftimg, rightimg, ph
       </div>
       <div className="w-full h-full flex justify-center items-end">
         {/* setting w,h for scale cover size img */}
-        <div className="group relative w-[430px] md:w-[800px] h-[288px] md:h-[450px] flex justify-center items-end overflow-hidden group">
+        <div className="group relative w-[430px] md:w-[800px] h-[288px] md:h-[448px] flex justify-center items-end overflow-hidden group">
           {/* setting w,h for size img */}
           <Image
-            className="z-20 w-full h-full max-w-[260px] md:max-w-[360px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] group-hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
+            className="z-20 w-full h-full max-w-[260px] md:max-w-[350px] max-h-[280px] md:max-h-[360px] transition-transform duration-[250ms] object-cover object-top drop-shadow-[-5px_0px_4px_rgba(0,0,0,0.4)] group-hover:scale-[1.02] hover:drop-shadow-[-1px_-1px_4px_rgba(0,0,0,0.7)]"
             src={mainimg} width={220} height={260} alt='proj_note' />
           <Image
-            className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[100px] md:w-[140px]  h-[230px] md:h-[300px] bottom-0 right-6 md:right-20 rotate-[18deg] transition-transform duration-[250ms]"
+            className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:rotate-[8deg] absolute w-[100px] md:w-[160px]  h-[230px] md:h-[280px] bottom-0 right-6 md:right-20 rotate-[18deg] transition-transform duration-[250ms]"
             src={leftimg} width={160} height={160} alt=""
           />
           <Image
-            className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[100px] md:w-[140px] md:h-[300px] h-[230px] -bottom-2  left-6 md:left-24 -rotate-[18deg] transition-transform duration-[250ms]"
+            className="blur-[1px] group-hover:blur-0 group-hover:scale-125 group-hover:-rotate-[10deg] absolute w-[100px] md:w-[140px] md:h-[280px] h-[230px] -bottom-2  left-6 md:left-24 -rotate-[18deg] transition-transform duration-[250ms]"
             src={rightimg} width={160} height={160} alt=""
           />
         </div>
